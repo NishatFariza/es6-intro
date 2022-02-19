5afc9ab38d414d1fb9d695c47b2cc65f09a44133
NishatFariza <nishatfarzia78@gmail.com>
2022-02-19 18:46:36 +0600

    basic practice done

--- basic.js
+++ basic.js
@@ -32,4 +32,19 @@ const numbers=[34, 56, 67, 12, 35, 87, 98, 65, 90];
 const arraylength =numbers.length;
 const newArrayAdd =numbers.push(34);
 const lastArrayRemove =numbers.pop();
-const sepcificArraychange =
+numbers[3]= 555;
+
+//loop
+// for loop;
+//for of loop
+
+//function
+function fullName(first, last){
+    const name = first + '' + last;
+    console.log(name);
+}
+fullName('nishat', 'fariza');
+
+
+//object
+const bottol ={color:yello, contain:TreeWalker, price:50}
