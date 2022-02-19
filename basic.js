f626b9063ffcac548f016397a52fe9d086eff969
NishatFariza <nishatfarzia78@gmail.com>
2022-02-19 18:24:36 +0600

    initial set up

---
+++ basic.js
@@ -0,0 +1,35 @@
+//variable
+var number =22;
+let changeAbleNumber =23;
+ const noChangeAbleNumber = 45;
+
+
+ //condition
+ if(deposit >500){
+
+ }
+ else if(deposit < 300){
+
+ }
+else if(deposit >= 200){
+
+}
+else if(deposit <= 100){
+
+}
+else if(deposit == 100){
+
+}
+else if(deposit !=600){
+
+}
+else{
+
+}
+
+//array
+const numbers=[34, 56, 67, 12, 35, 87, 98, 65, 90];
+const arraylength =numbers.length;
+const newArrayAdd =numbers.push(34);
+const lastArrayRemove =numbers.pop();
+const sepcificArraychange =
