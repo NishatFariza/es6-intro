3265e70c67ddad66b609548adf8cac9f28fc8e7c
NishatFariza <nishatfarzia78@gmail.com>
2022-02-21 05:32:30 +0600

    let-const practice

---
+++ let-const.js
@@ -0,0 +1,32 @@
+const balance = 1200;
+// balence =1340 ; [error]
+
+
+//let
+let balence =1200;
+// balance =1349;//[right]
+
+//ex
+const userName ='janpakhi';
+const together ='ami'+' ' + userName;
+//  console.log(together);
+
+//number
+const numbers =[45, 22, 33, 47, 87, 39];
+// numbers =[2, 8, 89, 65, 32, 12];//  error
+
+
+// object
+const student ={rool:20, name:'Nishat', job:'Intern'};
+// student.name ='Fariza';//right
+// student ={name:'sajid'};//error
+
+//for loop
+for (let i=0; i<numbers.length; i++){
+    const number= numbers[i]; //right
+    console.log(number);
+}
+
+
+
+
